b4ac10e1a82e4f8b59eb90463ef5905a5e898d2a
malyesh <74512928+malyesh@users.noreply.github.com>
2023-11-16 19:52:32 -0500

    create header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import ProductDetailPage from './pages/ProductDetailPage/ProductDetailPage';
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/home' element={<HomePage />} />

b4b7947a6a221794f454a91612d8653209909778
Jonathan Silva <jonathan.silva167@gmail.com>
2023-11-17 01:12:54 -0500

    refactor result card

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -3,29 +3,32 @@ import './ResultCard.scss';
 const ResultCard = () => {
     return (
         <article className='result-card'>
-            <div className='result-card__img-column'>
-                <img src="" alt="" className='result-card__img-column--img' />
-                <h2 className='result-card__img-column--price'></h2>
+            <div className='result-card__top'>
+                <h2 className='result-card__top--data-title'>{data.name}</h2>
             </div>
-            <div className="result-card__content-column">
-                <h2 className="result-card__content-column--result-title">Result Title</h2>
-                <h3 className="result-card__content-column--result-subtitle">Result Subtitle</h3>\
-                <p className="result-card__content-column--result-description">
-                    This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description.
+            <div className="result-card__detail">
+                <div className='result-card__detail-header'>
+                    <img src={data.logo} alt="company logo" className="result-card__detail--logo" />
+                    <h2 className="result-card__detail--logo-subtitle">By {data.company}</h2>
+                </div>
+                <p className="result-card__detail--description">
+                    This is a description. This is a description. This is a description. This is a description. This is a description. This is a description. This is a description.
                 </p>
             </div>
-            <aside className="result-card__bottom">
-                <div className="result-card__bottom-left">
-                    <p className="result-card__bottom-left--upload-date">Uploaded 11/16/2023</p>
-                    <p className="result-card__bottom-left--subscription-length">12 month subscription available</p>
+            <div className="result-card__price-contaier">
+                <p className="result-card__price-contaier--price">Uploaded {data.uploaded}</p>
+                <p className="result-card__price-contaier--subscription-length">{data.subscription}</p>
+                <div className="result-card__price-container--bottom">
+                    <p className="result-card__price-contaier--usability-score">Usability</p>
+                    {data.score}
+                    {data.stars}
                 </div>
-                <div className="result-card__bottom-right--expand-text">
-                    <p className="result-card__bottom-right--text">Click to Expand</p>
-                    <div className="result-card__bottom-right--img">
-                        <img src="" alt="" className="result-card__bottom-right--img" />
-                    </div>
-                </div>
-            </aside>
+            </div>
+            <div className="result-card__bottom">
+                <div className="result-card__bottom--published">Published:{data.published}</div>
+                <div className="result-card__bottom--updated">Updated:{data.updated}</div>
+                <div className="result-card__bottom--data-size">Data Size:{data.size}</div>
+            </div>
         </article>
     )
 }

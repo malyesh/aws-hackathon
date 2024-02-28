7deab3ff6f26724336c5bc918bb256f980b746d6
Jonathan Silva <jonathan.silva167@gmail.com>
2024-02-27 20:42:29 -0500

    add demo modal, and guided tour modals

--- src/App.js
+++ src/App.js
@@ -6,11 +6,48 @@ import Header from './components/Header/Header';
 import './App.scss';
 
 function App() {
-	const [loading, setLoading] = useState(true);
+	const [loading, setLoading] = useState(false);
+	const [tour, setTour] = useState({ tour: false, step: 0 });
+
+	const handleCloseModal = () => {
+		setLoading(false)
+	};
+
+	const handleTour = () => {
+		if (tour.tour === false && tour.step === 0) {
+			setTour(
+				{
+					tour: true,
+					step: 0
+				}
+			);
+			setLoading(false);
+		} else if (tour.step === 3) {
+			setTour(
+				{
+					tour: false,
+					step: 0
+				}
+			);
+		} else {
+			setTour(prevTour => ({
+				...prevTour,
+				step: prevTour.step + 1
+			}));
+		}
+	}
+
+	const handleTourReverse = () => {
+		setTour(prevTour => ({
+			...prevTour,
+			step: prevTour.step - 1
+		}));
+	}
+
 
 	useEffect(() => {
 		setTimeout(() => {
-			setLoading(false);
+			setLoading(true);
 		}, 2000);
 	}, []);
 
@@ -18,12 +55,14 @@ function App() {
 	return (
 		<div className='App'>
 
-			{
-				// loading ?
+			{loading ?
 				<div className="demo-modal">
 					<h2 className="demo-modal__h1">Attention! ⚠️</h2>
 					<p className="demo-modal__p">
 						This is a demo of my AWS Hackathon project. The data sets are real, but the A.I. (LLM) powered search function which was powered by a paid third party API has been disabled. The functionality of the search has been simulated.
+					</p>
+					<p className="demo-modal__p">
+						Use the query <strong>"animal"</strong>.
 					</p>
 					<h3 className="demo-modal__h2">Intended Functionality:</h3>
 					<p className="demo-modal__p">
@@ -32,15 +71,26 @@ function App() {
 					<p className="demo-modal__p">
 						Concurrent/async to the normal search, the A.I. powered search function uses the search query to look through the descriptions of all other datasets, and returns results which have high probability of correlation based on context and current events.
 					</p>
+					<button onClick={handleCloseModal} className="demo-modal__close">close</button>
+					<button onClick={handleTour} className="demo-modal__close">take tour</button>
 				</div>
-				//  : null
+				: null
 			}
+
+			{
+				tour.tour ?
+					<div className="darken">
+					</div>
+					: null
+			}
+
+
 
 			<BrowserRouter>
 				<Header />
 				<Routes>
-					<Route path='/' element={<HomePage />} />
-					<Route path='/home' element={<HomePage />} />
+					<Route path='/' element={<HomePage tour={tour} setTour={setTour} handleTour={handleTour} handleTourReverse={handleTourReverse} />} />
+					{/* <Route path='/home' element={<HomePage />} /> */}
 					<Route path='/product' element={<ProductDetailPage />} />
 				</Routes>
 			</BrowserRouter>
--- src/components/SearchComponent/SearchComponent.jsx
+++ src/components/SearchComponent/SearchComponent.jsx
@@ -1,79 +1,105 @@
 import './SearchComponent.scss';
 
-export const SearchComponent = ({ handleSubmit }) => {
-  return (
-    <div className='search'>
-      <h3 className='search__title'>Browse Catalog</h3>
-      <form onSubmit={handleSubmit}>
-        <section className='search__input-row'>
-          <div className='search__wrap'>
-            <select className='search__select' name='category'>
-              <option value='categories' name='categories' disabled selected>
-                Categories
-              </option>
-              <option value='automotive' name='automotive'>
-                Automotive
-              </option>
-              <option value='environmental' name='environmental'>
-                Environmental
-              </option>
-              <option value='financial-services' name='financial-services'>
-                Financial Services
-              </option>
-              <option value='gaming' name='gaming'>
-                Gaming
-              </option>
-              <option
-                value='healthcare-life-sciences'
-                name='healthcare-life-sciences'
-              >
-                Healthcare & Life Sciences
-              </option>
-              <option value='manufacturing' name='manufacturing'>
-                Manufacturing
-              </option>
-              <option value='media-entertainment' name='media-entertainment'>
-                Media & Entertainment
-              </option>
-              <option value='public-sector' name='public-sector'>
-                Public Sector
-              </option>
-              <option value='resources' name='resources'>
-                Resources
-              </option>
-              <option
-                value='retail-location-marketing'
-                name='retail-location-marketing'
-              >
-                Retail, Location & Marketing
-              </option>
-              <option value='telecommunications' name='telecommunications'>
-                Telecommunications
-              </option>
-            </select>
-            <input
-              type='text'
-              placeholder='search'
-              className='search__input'
-              name='searchInput'
-            />
-          </div>
-          <button type='submit' className='search__button'>
-            Search
-          </button>
-        </section>
-        <section className='search__bottom'>
-          <p className='search__bottom--breadcrumbs'>{'<  1 2 3 ... >'}</p>
-          <select className='search__bottom-select'>
-            <option value='' disabled selected>
-              Sort By
-            </option>
-            <option value='recentlyUploaded'>Sort By: Recently Uploaded</option>
-            <option value='dataSize'>Sort By: Data Size</option>
-            <option value='popularity'>Sort By: Popularity</option>
-          </select>
-        </section>
-      </form>
-    </div>
-  );
+export const SearchComponent = ({ handleSubmit, tour, setTour }) => {
+	return (
+		<div className={`search ${tour.tour && tour.step <= 1 ? 'tour-highlight' : ''}`}>
+			<h3 className='search__title'>Browse Catalog</h3>
+			<form onSubmit={handleSubmit}>
+				<section className='search__input-row'>
+					<div className='search__wrap'>
+						<select className='search__select' name='category'>
+							<option value='categories' name='categories' disabled selected>
+								Categories
+							</option>
+							<option value='automotive' name='automotive'>
+								Automotive
+							</option>
+							<option value='environmental' name='environmental'>
+								Environmental
+							</option>
+							<option value='financial-services' name='financial-services'>
+								Financial Services
+							</option>
+							<option value='gaming' name='gaming'>
+								Gaming
+							</option>
+							<option
+								value='healthcare-life-sciences'
+								name='healthcare-life-sciences'
+							>
+								Healthcare & Life Sciences
+							</option>
+							<option value='manufacturing' name='manufacturing'>
+								Manufacturing
+							</option>
+							<option value='media-entertainment' name='media-entertainment'>
+								Media & Entertainment
+							</option>
+							<option value='public-sector' name='public-sector'>
+								Public Sector
+							</option>
+							<option value='resources' name='resources'>
+								Resources
+							</option>
+							<option
+								value='retail-location-marketing'
+								name='retail-location-marketing'
+							>
+								Retail, Location & Marketing
+							</option>
+							<option value='telecommunications' name='telecommunications'>
+								Telecommunications
+							</option>
+						</select>
+						<input
+							type='text'
+							placeholder='search'
+							className='search__input'
+							name='searchInput'
+						/>
+
+						{tour.tour && tour.step === 0 ?
+							<div
+								style={{ bottom: '125%', left: '240px' }}
+								class="tooltiptext"
+							>
+								Use "animal" as your search term</div>
+							: null
+						}
+
+					</div>
+					<button type='submit' className='search__button'>
+						Search
+						{tour.tour && tour.step === 1 ?
+							<div
+								style={{
+									bottom: '125%',
+									width: '114px',
+									left: '-5px'
+								}}
+								class="tooltiptext"
+							>
+								Submit your search query
+							</div>
+							: null
+						}
+					</button>
+				</section>
+				<section className='search__bottom'>
+					<p className='search__bottom--breadcrumbs'>{'<  1 2 3 ... >'}</p>
+					<select className='search__bottom-select'>
+						<option value='' disabled selected>
+							Sort By
+						</option>
+						<option value='recentlyUploaded'>Sort By: Recently Uploaded</option>
+						<option value='dataSize'>Sort By: Data Size</option>
+						<option value='popularity'>Sort By: Popularity</option>
+					</select>
+				</section>
+			</form>
+			{
+
+			}
+		</div>
+	);
 };
